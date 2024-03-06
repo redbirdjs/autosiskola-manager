60bc9df62bbd4233762dd1352ea5c97a2de5b3a3
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-06 18:29:02 +0100

    feat: added register page

---
+++ app/register/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
+
+export default function RegisterPage() {
+  return (
+    <main className='flex min-h-screen flex-col items-center p-24'>
+      <h1 className='text-4xl mb-10'>Register Page</h1>
+      <Link href={'/'} className={buttonVariants({ variant: 'default' })}>Back To Main Page</Link>
+    </main>
+  )
+}
