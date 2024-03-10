8ce4dcf9628f213e30c199ddb62a386763d7cbad
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-10 20:54:10 +0100

    refactor(errors): register from only shows first error

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -9,6 +9,7 @@ import { register } from '@/utils/actions'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp'
+import { STATIC_STATUS_PAGES } from 'next/dist/shared/lib/constants'
 
 export default function RegisterForm() {
   const initialState = { message: '', errors: {} };
@@ -19,29 +20,29 @@ export default function RegisterForm() {
       <label htmlFor="username" className='mb-2'>Username <Required /></label>
       <Input name='username' className='mb-3' />
       {
-        state.errors?.username && state.errors.username.map(error => (
-          <div key={error}>
-            <p className='text-red-600'>{ error }</p>
+        state.errors?.username && (
+          <div>
+            <p className='text-red-600'>{ state.errors.username[0] }</p>
           </div>
-        ))
+        )
       }
       <label htmlFor="realname" className='mb-2'>Full Name <Required /></label>
       <Input name='realname' className='mb-3' />
       {
-        state.errors?.realname && state.errors.realname.map(error => (
-          <div key={error}>
-            <p className='text-red-600'>{ error }</p>
+        state.errors?.realname && (
+          <div>
+            <p className='text-red-600'>{ state.errors.realname[0] }</p>
           </div>
-        ))
+        )
       }
       <label htmlFor="email" className='mb-2'>Email address <Required /></label>
       <Input name='email' className='mb-3' />
       {
-        state.errors?.email && state.errors.email.map(error => (
-          <div key={error}>
-            <p className='text-red-600'>{ error }</p>
+        state.errors?.email && (
+          <div>
+            <p className='text-red-600'>{ state.errors.email[0] }</p>
           </div>
-        ))
+        )
       }
       <label htmlFor='passport' className='mb-2'>Passport Number <Required /></label>
       <InputOTP className='mb-3' name='passport' pattern={Pattern} maxLength={9} render={({ slots }) => (
@@ -54,29 +55,29 @@ export default function RegisterForm() {
         </>
       )} />
       {
-        state.errors?.passport && state.errors.passport.map(error => (
-          <div key={error}>
-            <p className='text-red-600'>{ error }</p>
+        state.errors?.passport && (
+          <div>
+            <p className='text-red-600'>{ state.errors.passport[0] }</p>
           </div>
-        ))
+        )
       }
       <label htmlFor="pass1" className='mb-2'>Password <Required /></label>
       <Input name='pass1' type='password' className='mb-3' />
       {
-        state.errors?.pass1 && state.errors.pass1.map(error => (
-          <div key={error}>
-            <p className='text-red-600'>{ error }</p>
+        state.errors?.pass1 && (
+          <div>
+            <p className='text-red-600'>{ state.errors.pass1[0] }</p>
           </div>
-        ))
+        )
       }
       <label htmlFor="pass2" className='mb-2'>Repeat password <Required /></label>
       <Input name='pass2' type='password' className='mb-3' />
       {
-        state.errors?.pass2 && state.errors.pass2.map(error => (
-          <div key={error}>
-            <p className='text-red-600'>{ error }</p>
+        state.errors?.pass2 && (
+          <div>
+            <p className='text-red-600'>{ state.errors.pass2[0] }</p>
           </div>
-        ))
+        )
       }
       <Button type='submit' variant={'default'} className='my-5 self-center w-[50%]'>Register</Button>
     </form>
