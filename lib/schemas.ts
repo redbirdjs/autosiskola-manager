57784e3d214479778bbf41daf4de421289c2cab4
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-10 14:23:37 +0100

    feat: added register and login schemas

---
+++ lib/schemas.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+
+export const RegisterSchema = z.object({
+  username: z.string().min(1, 'This field is required!').min(5, 'Username must be 5 characters long minimum!'),
+  realname: z.string().min(1, 'This field is required!'),
+  email: z.string().min(1, 'This field is required!').email({ message: 'Invalid email format!' }),
+  passport: z.string().min(1, 'This field is required!').regex(/^[A-Z0-9]{8,9}$/, 'Invalid passport number!').length(8, 'The ID must be 8 characters long.'),
+  pass1: z.string().min(1, 'This field is required!').min(8, 'The password must be at least 8 characters long.'),
+  pass2: z.string().min(1, 'This field is required!'),
+});
+
+export const LoginSchema = z.object({
+  email: z.string().email({ message: 'Invalid email format!' }),
+  password: z.string().min(8),
+});
