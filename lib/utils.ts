adbd6fbec6d1408530b16fa57f6b5a5129fb470c
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-10 21:21:12 +0100

    feat: added string generator

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function randomString(length: number, filter: RegExp = /[A-Za-z0-9]/g): string {
+  let str = '';
+  let chars = 'aA0bB1cC2dD3eE4fF5gG6hH7iI8jJ9kKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ';
+
+  for (let i = 0; i < length; i++) {
+    if (chars.match(filter)) {
+      str += chars.charAt(Math.floor(Math.random() * chars.length));
+    } else {
+      i--;
+    }
+  }
+
+  return str;
 }
