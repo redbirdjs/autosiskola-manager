4ff69b6ef2432fcd6e54a31c6516e285a64c565c
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-10 21:24:15 +0100

    chore: register schema updated

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -4,7 +4,7 @@ export const RegisterSchema = z.object({
   username: z.string().min(1, 'This field is required!').min(5, 'Username must be 5 characters long minimum!'),
   realname: z.string().min(1, 'This field is required!'),
   email: z.string().min(1, 'This field is required!').email({ message: 'Invalid email format!' }),
-  passport: z.string().min(1, 'This field is required!').regex(/^[A-Z0-9]{8,9}$/, 'Invalid passport number!').length(8, 'The ID must be 8 characters long.'),
+  passport: z.string().min(1, 'This field is required!').regex(/^[A-Z0-9]{8,9}$/, 'Invalid passport number!'),
   pass1: z.string().min(1, 'This field is required!').min(8, 'The password must be at least 8 characters long.'),
   pass2: z.string().min(1, 'This field is required!'),
 });
