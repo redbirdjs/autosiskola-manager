8be102e7e7ce742cd02a899c768b749b861549c1
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-12 18:53:52 +0100

    feat: added check for logged in user

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,18 @@
 import { Metadata } from 'next'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Dashboard'
 }
 
 export default function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const cookieStore = cookies();
+
+  if (!cookieStore.get('refreshToken')) {
+    redirect('/');  
+  }
+
   return (
     <>
       { children }
