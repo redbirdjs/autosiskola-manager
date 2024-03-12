938700554772c2e46d243ae0330b6c9750fc4d9e
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-12 20:42:04 +0100

    chore: moved log out button to its own component

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { logout } from '@/utils/actions'
+
+export default function LogoutButton() {
+  const logOut = () => {
+    logout();
+  }
+
+  return (
+    <Button onClick={logOut}>Logout</Button>
+  )
+}
