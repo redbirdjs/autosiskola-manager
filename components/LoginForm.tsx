e6622cd69f13a9742186fc29a778601f1c94f4f5
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-12 17:53:42 +0100

    feat: added login form

---
+++ components/LoginForm.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { clsx } from 'clsx'
+import { useEffect } from 'react'
+import { useFormState } from 'react-dom'
+
+import { login } from '@/utils/actions'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+export default function LoginForm({ setMsg }: { setMsg: Function }) {
+  const initialState = { message: { title: '' }, errors: {} };
+  const [state, dispatch] = useFormState(login, initialState);
+
+  useEffect(() => {
+    setMsg(state.message);
+  }, [state, setMsg]);
+
+  return (
+    <form action={dispatch} className='flex flex-col'>
+      <label htmlFor='email'>Email address</label>
+      <Input name='email' type='email' className={clsx({ 'border-red-600': state.errors?.email, 'mb-3': !state.errors?.email })} />
+      {
+        state.errors?.email && (
+          <div aria-live='polite' aria-atomic>
+            <p className='text-red-600'>{ state.errors.email[0] }</p>
+          </div>
+        )
+      }
+      <label htmlFor='password'>Password</label>
+      <Input name='password' type='password' className={clsx({ 'border-red-600': state.errors?.password }, 'mb-1')} />
+      {
+        state.errors?.password && (
+          <div aria-live='polite' aria-atomic>
+            <p className='text-red-600'>{ state.errors.password[0] }</p>
+          </div>
+        )
+      }
+      <p className='mb-3 underline'><a href='/forgotten-password'>Forgot password?</a></p>
+      <Button type='submit' variant={'default'} className='my-5 self-center w-[50%]'>Login</Button>
+    </form>
+  )
+}
