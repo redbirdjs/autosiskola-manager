70cd696cb631402e22d6c6d4f6ac47e9ccc6a23e
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-12 18:54:35 +0100

    feat: moved page to client side, added logout btn

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,20 @@
+'use client'
+
 import Link from 'next/link'
-import { buttonVariants } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
+
+import { logout } from '@/utils/actions'
 
 export default function DashboardPage() {
+  const logOut = () => {
+    logout();
+  }
+
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
       <h1 className='text-4xl mb-10'>Dashboard Page</h1>
-      <Link href={'/'} className={buttonVariants({ variant: 'default' })}>Back To Main Page</Link>
+      <Link href={'/'} className={buttonVariants({ variant: 'default' }) + ' mb-1'}>Back To Main Page</Link>
+      <Button onClick={logOut}>Logout</Button>
     </main>
   );
 }
