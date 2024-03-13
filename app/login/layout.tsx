924fe1cbfb07e07602928727cb54b18ccc8e1bc2
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-13 19:19:29 +0100

    feat: redirect to dashboard if user logged in

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,10 +1,16 @@
 import { Metadata } from 'next'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Login'
 }
 
 export default function LoginLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  if (cookies().has('refreshToken')) {
+    redirect('/dashboard');
+  }
+
   return (
     <>
       { children }
--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,10 +1,16 @@
 import { Metadata } from 'next'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Register'
 }
 
 export default function RegisterLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  if (cookies().has('refreshToken')) {
+    redirect('/dashboard');
+  }
+
   return (
     <>
       { children }
