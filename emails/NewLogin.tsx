aa649f201d78ea148c02a133e062288f922245f8
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-16 16:22:43 +0100

    style: made styling changes in newlogin mail

--- emails/NewLogin.tsx
+++ emails/NewLogin.tsx
@@ -18,10 +18,18 @@ export default function Email({ address, userAgent }: NewLoginProps) {
               <Img src={`${process.env.SITE_URL || 'http://localhost:3000'}/vercel.svg`} height={50} alt='Logo of the Driving School Manager' className='my-0 mx-auto text-center' />
             </Section>
             <Section>
-              <Text>A new login was detected from <strong>{ address } ({ userAgent })</strong></Text>
+              Your security is very important to us. This account was recently accessed from this IP address:
             </Section>
             <Section>
-              <Text className='text-[#666666] text-sm text-justify'>If it wasn&apos;t you who logged in, please change your password, be sure not to use the same password as for your email address. Changing your password will <span className='text-black font-bold'>unlink all devices from your account</span>. If you no longer have access to your account contact us via email.</Text>
+              <Heading as='h2' className='border-b border-solid border-b-black border-t-transparent border-x-transparent'>Details</Heading>
+              <Text>
+                <strong>Time:</strong> { (new Date()).toUTCString() }<br/>
+                <strong>IP address:</strong> { address }<br/>
+                <strong>Browser:</strong> { userAgent }
+              </Text>
+            </Section>
+            <Section>
+              <Text className='text-[#666666] text-sm text-justify'>If this was you, you can safely ignore this email. If you noticed any suspicious activity on your account, please change your password. If you have any questions or concerns, don&apos;t be affraid to contact us.</Text>
             </Section>
           </Container>
         </Body>
