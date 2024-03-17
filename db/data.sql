62e981dc0425fcb4177e47a5988bb76fed7cbf4e
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-17 11:41:50 +0100

    chore: changed rank name length to 9 changed rank names in db file

--- db/data.sql
+++ db/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO ranks (name) VALUES ('Tanuló'), ('Oktató'), ('Igazgató') ON CONFLICT DO NOTHING;
+INSERT INTO ranks (name) VALUES ('Student'), ('Teacher'), ('Principal') ON CONFLICT DO NOTHING;
 INSERT INTO categories (category) VALUES ('A'), ('B'), ('C'), ('D'), ('BE'), ('CE'), ('DE') ON CONFLICT DO NOTHING;
