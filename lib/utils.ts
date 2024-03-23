55e0bc73c5881ec5947ba9b39a8c044324ad3b7c
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-23 10:09:15 +0100

    feat: added star rating generation function

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,3 +19,11 @@ export function randomString(length: number, filter: RegExp = /[A-Za-z0-9]/g): s
 
   return str;
 }
+
+export function generateRating(sum: number, total: number) {
+  let fullStars = Math.floor(sum / total);
+  let halfStars = (sum % total != 0) ? 1 : 0;
+  let emptyStars = (5 - fullStars);
+
+  return [fullStars, halfStars, emptyStars];
+}
