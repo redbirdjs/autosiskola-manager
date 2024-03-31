213455940a9120c88d0725a7a6f68a403ae835fb
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-03-31 13:21:56 +0200

    feat: created a search bar

---
+++ components/dashboard/Search.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useDebouncedCallback } from 'use-debounce'
+import { useSearchParams, useRouter, usePathname } from 'next/navigation'
+import { Search } from 'lucide-react'
+
+import { Input } from '@/components/ui/input'
+
+export default function SearchField() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  return (
+    <div className='relative flex w-1/5'>
+      <label htmlFor='searchbar' className='sr-only'>Search</label>
+      <Input id='searchbar' placeholder='Search...' onChange={(e) => handleSearch(e.target.value)} defaultValue={searchParams.get('query')?.toString()} className='peer block py-[9px] pl-10 text-base' />
+      <Search className='absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
+    </div>
+  )
+}
