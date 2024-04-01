71113b68e71193cdc7e6da7959820203398bae45
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-01 13:26:46 +0200

    style: moved logo to the left on phone view, changed text size to smaller

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -11,14 +11,14 @@ export default async function Header() {
 
   return (
     <header className='flex flex-row justify-end px-10 py-5 border-b border-b-[#eaeaea]'>
-      <Image src={Logo} alt='DSM Logo' width={55} className='absolute left-1/2 -translate-x-1/2' />
+      <Image src={Logo} alt='DSM Logo' width={55} className='max-md:mr-auto md:absolute md:left-1/2 md:-translate-x-1/2' />
       <div className='flex flex-row items-center gap-2'>
         <div className='flex flex-row gap-4 items-center p-2 rounded-md'>
           <div className='flex flex-col'>
-            <p className='text-lg'>{ user?.realname }</p>
+            <p className='text-base'>{ user?.realname }</p>
             <Badge className='self-center'>{ user?.rank }</Badge>
           </div>
-          { user?.avatarPath.endsWith('default.svg') && <UserRound className='h-12 w-12 rounded-full text-white bg-black p-2' /> || <Image src={`${user?.avatarPath}`} width={42} height={42} className='rounded-full' priority={true} alt={`${user?.username}'s profile avatar`} /> }
+          { user?.avatarPath && user?.avatarPath.endsWith('default.svg') && <UserRound className='h-12 w-12 rounded-full text-white bg-black p-2' /> || <Image src={`${user?.avatarPath}`} width={42} height={42} className='rounded-full' priority={true} alt={`${user?.username}'s profile avatar`} /> }
         </div>
         <LogoutButton />
       </div>
