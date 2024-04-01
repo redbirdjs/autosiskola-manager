a970984c0b8a04043c9597bfdbebf9579d940f4d
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-01 11:44:38 +0200

    style: fixed not found page width

--- app/dashboard/[...notFound]/page.tsx
+++ app/dashboard/[...notFound]/page.tsx
@@ -2,7 +2,7 @@ import { Unplug } from 'lucide-react'
 
 export default function NotFound() {
   return (
-    <div className='w-full flex items-center justify-center gap-4'>
+    <div className='w-10/12 flex items-center justify-center gap-4'>
       <Unplug className='w-20 h-20' /> 
       <div className='flex flex-col items-center justify-center'>
         <h1 className='flex flex-row gap-4 items-center text-6xl'>404</h1>
