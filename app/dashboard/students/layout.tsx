2165f26d753937bb12e85692e3c2ca93079a15b0
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-01 13:27:20 +0200

    feat: created students list page

---
+++ app/dashboard/students/layout.tsx
@@ -0,0 +1,24 @@
+import { Suspense } from 'react'
+import { Metadata } from 'next'
+
+import DynamicBreadcrumb from '@/components/dashboard/DynamicBreadcrumb'
+import Search from '@/components/dashboard/Search'
+import { UserCardSkeleton } from '@/components/skeletons/skeletons'
+
+export const metadata: Metadata = {
+  title: 'Students | Dashboard'
+}
+
+export default async function StudentsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className='flex flex-col p-5 w-10/12'>
+      <div className='flex justify-between items-center mb-5'>
+        <DynamicBreadcrumb />
+        <Search />
+      </div>
+      <Suspense fallback={<UserCardSkeleton/>}>
+        { children }
+      </Suspense>
+    </main>
+  );
+}
---
+++ app/dashboard/students/page.tsx
@@ -0,0 +1,35 @@
+import { SearchX } from 'lucide-react'
+
+import DynamicPagination from '@/components/dashboard/Pagination'
+import UserCard from '@/components/dashboard/UserCard'
+import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert'
+
+import { getFilteredUsers } from '@/utils/actions'
+
+export default async function StudentsPage({ searchParams }: { searchParams: { query: string, page: string } }) {
+  const query = searchParams?.query || '';
+  const page = searchParams?.page || '1';
+
+  const searchResults = await getFilteredUsers({ query, page, rankType: 'student' });
+
+  return (
+    <div>
+      {
+        searchResults.users.length != 0 ? searchResults.users.map((user) => (
+          <UserCard key={user.username} user={user} />
+        )) : (
+          <div className='flex justify-center p-10'>
+            <Alert className='w-max'>
+              <SearchX className='w-5 h-5' />
+              <AlertTitle>No users found!</AlertTitle>
+              <AlertDescription>
+                No users found for the specified query. Please try another one.
+              </AlertDescription>
+            </Alert>
+          </div>
+        )
+      }
+      <DynamicPagination currentPage={page} pages={searchResults.pages}  />
+    </div>
+  )
+}
