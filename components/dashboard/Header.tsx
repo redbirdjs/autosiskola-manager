7d383bf60dd78837ad1da07478d5152f48aa2a97
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-01 11:46:33 +0200

    chore: changed username to realname

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -15,8 +15,8 @@ export default async function Header() {
       <div className='flex flex-row items-center gap-2'>
         <div className='flex flex-row gap-4 items-center p-2 rounded-md'>
           <div className='flex flex-col'>
-            <p className='text-lg'>{ user?.username }</p>
-            <Badge className='self-start'>{ user?.rank }</Badge>
+            <p className='text-lg'>{ user?.realname }</p>
+            <Badge className='self-center'>{ user?.rank }</Badge>
           </div>
           { user?.avatarPath.endsWith('default.svg') && <UserRound className='h-12 w-12 rounded-full text-white bg-black p-2' /> || <Image src={`${user?.avatarPath}`} width={42} height={42} className='rounded-full' priority={true} alt={`${user?.username}'s profile avatar`} /> }
         </div>
