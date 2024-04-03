9c8805fa3e51055a22d49f6ab9b865171446632f
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-03 09:16:50 +0200

    style: changed avatar size

--- components/skeletons/skeletons.tsx
+++ components/skeletons/skeletons.tsx
@@ -4,7 +4,7 @@ export function UserCardSkeleton() {
   return (
     <div className='flex flex-row gap-5 border border-[#eaeaea] rounded-lg p-5'>
       <div className='flex space-x-4 animate-pulse'>
-        <div className='w-[75px] h-[75px] rounded-full bg-gray-300'></div>
+        <div className='w-[35px] h-[35px] rounded-full bg-gray-300'></div>
         <div className='flex flex-col gap-1 justify-center'>
           <div className='w-[400px] h-[28px] bg-gray-300 rounded-md'></div>
           <div className='w-[300px] h-[21px] bg-gray-300 rounded-md'></div>
