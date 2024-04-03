337ae4857a92b649fd489c8010a8f93dd6070c4a
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-03 17:30:35 +0200

    feat: added className prop to search component

--- components/dashboard/Search.tsx
+++ components/dashboard/Search.tsx
@@ -1,12 +1,13 @@
 'use client'
 
+import clsx, { ClassValue } from 'clsx'
 import { useDebouncedCallback } from 'use-debounce'
 import { useSearchParams, useRouter, usePathname } from 'next/navigation'
 import { Search } from 'lucide-react'
 
 import { Input } from '@/components/ui/input'
 
-export default function SearchField() {
+export default function SearchField({ className }: { className?: string | ClassValue[] }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -23,7 +24,7 @@ export default function SearchField() {
   }, 300);
 
   return (
-    <div className='relative flex w-1/5'>
+    <div className={clsx('relative flex w-1/5', className)}>
       <label htmlFor='searchbar' className='sr-only'>Search</label>
       <Input id='searchbar' placeholder='Search...' onChange={(e) => handleSearch(e.target.value)} defaultValue={searchParams.get('query')?.toString()} className='peer block py-[9px] pl-10 text-base' />
       <Search className='absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
