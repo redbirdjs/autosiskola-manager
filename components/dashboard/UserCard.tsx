70d678c1de819a46d46785812a92ad4deba45cab
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-03 09:17:31 +0200

    style: changed own image to avatar component

--- components/dashboard/UserCard.tsx
+++ components/dashboard/UserCard.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import { UserRound } from 'lucide-react'
 
-import { badgeVariants } from '@/components/ui/badge';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import { badgeVariants } from '@/components/ui/badge'
 import clsx from 'clsx';
 
 interface UserData {
@@ -15,13 +16,10 @@ interface UserData {
 export default function UserCard({ user }: { user: UserData }) {
   return (
     <div className='flex flex-row gap-5 border border-[#eaeaea] rounded-lg mb-3 p-5 hover:bg-gray-100 hover:border-gray-300 transition-colors'>
-      { user.path ? (
-        <Image src={user.path} alt='User Image' width={75} height={75} className='rounded-full flex items-center text-center border border-[#eaeaea]' />
-      ) : (
-        <div className='w-[75px] h-[75px] rounded-full flex items-center justify-center border border-[#eaeaea]'>
-          <UserRound className='self-center text-gray-400' size={40} />
-        </div>
-      ) }
+      <Avatar className='border border-gray-300'>
+        <AvatarImage src={ user.path } />
+        <AvatarFallback>{ user.realname.split(' ').map(x => x[0]).join('') }</AvatarFallback>
+      </Avatar>
       <div className='flex flex-col gap-1 justify-center'>
         <h1 className='text-2xl'>{ user.realname } ({ user.username })</h1>
         <p className='text-[#a0a0a0] text-base'>{ user.email }</p>
