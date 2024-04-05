10d7465143dae9a8ff395ea0f90d33d6c6501c43
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-05 21:40:52 +0200

    feat: created new vehicle sheet component

---
+++ components/dashboard/vehicles/NewVehicleSheet.tsx
@@ -0,0 +1,23 @@
+import { Plus, Car } from 'lucide-react'
+import { getCategories } from '@/utils/actions'
+
+import { Sheet, SheetContent,SheetHeader, SheetFooter, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { buttonVariants } from '@/components/ui/button'
+import NewVehicleForm from '@/components/dashboard/vehicles/NewVehicleForm'
+
+export default async function NewVehicleSheet() {
+  const categories = await getCategories();
+
+  return (
+    <Sheet>
+      <SheetTrigger className={ buttonVariants({ variant: 'default' }) }><Plus className='h-5 w-5' /> New</SheetTrigger>
+      <SheetContent>
+        <SheetHeader className='mb-3'>
+          <SheetTitle className='flex gap-2'><Car className='h-6 w-6' />New Vehicle</SheetTitle>
+          <hr />
+        </SheetHeader>
+        <NewVehicleForm categories={categories} />
+      </SheetContent>
+    </Sheet>
+  );
+}
