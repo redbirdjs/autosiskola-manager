873975e769fa96d97c4faad30f0719cb9fbba6d0
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-06 09:30:52 +0200

    style: added plus icon before car in header

--- components/dashboard/vehicles/NewVehicleSheet.tsx
+++ components/dashboard/vehicles/NewVehicleSheet.tsx
@@ -1,7 +1,7 @@
 import { Plus, Car } from 'lucide-react'
 import { getCategories } from '@/utils/actions'
 
-import { Sheet, SheetContent,SheetHeader, SheetFooter, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { Sheet, SheetContent,SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 import { buttonVariants } from '@/components/ui/button'
 import NewVehicleForm from '@/components/dashboard/vehicles/NewVehicleForm'
 
@@ -13,7 +13,7 @@ export default async function NewVehicleSheet() {
       <SheetTrigger className={ buttonVariants({ variant: 'default' }) }><Plus className='h-5 w-5' /> New</SheetTrigger>
       <SheetContent>
         <SheetHeader className='mb-3'>
-          <SheetTitle className='flex gap-2'><Car className='h-6 w-6' />New Vehicle</SheetTitle>
+          <SheetTitle className='flex items-center gap-2'><Plus className='h-5 w-5' /><Car className='h-6 w-6' />New Vehicle</SheetTitle>
           <hr />
         </SheetHeader>
         <NewVehicleForm categories={categories} />
