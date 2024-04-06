e4969cc109c559df8500b72bd4e0234116c974df
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-06 13:43:53 +0200

    style: added truncate for vehicle name

--- components/dashboard/VehicleCard.tsx
+++ components/dashboard/VehicleCard.tsx
@@ -22,7 +22,7 @@ export function VehicleCard({ vehicle }: { vehicle: VehicleData }) {
     <div className='flex flex-row items-center gap-5 border border-[#eaeaea] rounded-lg mb-3 p-5 hover:bg-gray-100 hover:border-gray-300 transition-colors'>
       <div className='mr-auto flex flex-row items-center gap-3'>
         <Image src={vehicle?.path} alt='Vehicle Image' width={70} height={70} className='mr-10' />
-        <div className='flex flex-col gap-1 justify-center'>
+        <div className='flex flex-col gap-1 justify-center truncate'>
           <h1 className='text-2xl'>{ vehicle?.brand } { vehicle?.type }</h1>
           <p className='text-[#a0a0a0] text-base'>{ vehicle?.plate }</p>
         </div>
