9ff19cc2866e3a9a294688dcdcfdeb5cc8bf0500
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-06 17:17:38 +0200

    feat: added back to dashboard button to error page

--- app/dashboard/error.tsx
+++ app/dashboard/error.tsx
@@ -1,14 +1,21 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import { Bomb } from 'lucide-react'
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }, reset: () => void }) {
+  const router = useRouter();
+  
   useEffect(() => {
     console.error(error);
   }, [error]);
+
+  const redirectDashboard = () => {
+    router.push('/dashboard');
+  }
 
   return (
     <div className='flex flex-col gap-5 w-full items-center justify-center p-5'>
@@ -18,6 +25,7 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
         <AlertDescription>There was an error while trying to fetch the user data!</AlertDescription>
       </Alert>
       <Button variant={'default'} onClick={reset}>Try Again</Button>
+      <Button variant={'default'} onClick={redirectDashboard}>Back to the Dasboard</Button>
     </div>
   )
 }
