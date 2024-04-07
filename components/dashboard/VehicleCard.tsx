9077ecb707b5815f8bd5f3d1f6763b0151477a6b
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-07 11:44:47 +0200

    feat: added modifyvehicle sheet to vehicle card

--- components/dashboard/VehicleCard.tsx
+++ components/dashboard/VehicleCard.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import Image from 'next/image'
-import { PencilRuler } from 'lucide-react'
 
 import DeleteVehicle from '@/components/dashboard/vehicles/DeleteVehicle'
+import ModifyVehicleSheet from '@/components/dashboard/vehicles/ModifyVehicleSheet'
 import { Tooltip, TooltipTrigger, TooltipProvider, TooltipContent } from '@/components/ui/tooltip'
 import { buttonVariants } from '@/components/ui/button'
 import { VehicleData } from '@/lib/definitions'
@@ -12,23 +12,25 @@ export default function VehicleCard({ vehicle }: { vehicle: VehicleData }) {
   return (
     <div className='flex flex-row items-center gap-5 border border-[#eaeaea] rounded-lg mb-3 p-5 hover:bg-gray-100 hover:border-gray-300 transition-colors'>
       <div className='mr-auto flex flex-row items-center gap-3'>
-        <Image src={vehicle?.path} alt='Vehicle Image' width={70} height={70} className='mr-10' />
+        <Image src={vehicle.path} alt='Vehicle Image' width={70} height={70} className='mr-10' />
         <div className='flex flex-col gap-1 justify-center truncate'>
-          <h1 className='text-2xl'>{ vehicle?.brand } { vehicle?.type }</h1>
-          <p className='text-[#a0a0a0] text-base'>{ vehicle?.plate }</p>
+          <h1 className='text-2xl'>{ vehicle.brand } { vehicle?.type }</h1>
+          <p className='text-[#a0a0a0] text-base'>{ vehicle.plate }</p>
         </div>
         <div>
-          <p className='text-lg px-4 py-2 bg-black text-white rounded-lg'>{ vehicle?.category }</p>
+          <p className='text-lg px-4 py-2 bg-black text-white rounded-lg'>{ vehicle.category }</p>
         </div>
       </div>
       <div className='flex flex-wrap gap-1'>
         <TooltipProvider>
           <Tooltip>
-            <TooltipTrigger className={buttonVariants({ variant: 'default' })}><PencilRuler className='h-5 w-5' /></TooltipTrigger>
+            <TooltipTrigger className={buttonVariants({ variant: 'default' })} asChild>
+              <ModifyVehicleSheet vehicle={vehicle} />
+            </TooltipTrigger>
             <TooltipContent>Modify</TooltipContent>
           </Tooltip>
           <Tooltip>
-            <DeleteVehicle plate={vehicle?.plate} />
+            <DeleteVehicle plate={vehicle.plate} />
             <TooltipContent>Delete</TooltipContent>
           </Tooltip>
         </TooltipProvider>
