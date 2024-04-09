239d4cae25e803715cca8404c9506ecbfccb55f9
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-09 06:40:58 +0200

    style: moved category next to brand, moved tooltiptrigger to modify sheet

--- components/dashboard/VehicleCard.tsx
+++ components/dashboard/VehicleCard.tsx
@@ -6,27 +6,27 @@ import DeleteVehicle from '@/components/dashboard/vehicles/DeleteVehicle'
 import ModifyVehicleSheet from '@/components/dashboard/vehicles/ModifyVehicleSheet'
 import { Tooltip, TooltipTrigger, TooltipProvider, TooltipContent } from '@/components/ui/tooltip'
 import { buttonVariants } from '@/components/ui/button'
-import { VehicleData } from '@/lib/definitions'
+import { VehicleData, CategoryName } from '@/lib/definitions'
 
-export default function VehicleCard({ vehicle }: { vehicle: VehicleData }) {
+export default function VehicleCard({ vehicle, categories }: { vehicle: VehicleData, categories: CategoryName[] }) {
   return (
     <div className='flex flex-row items-center gap-5 border border-[#eaeaea] rounded-lg mb-3 p-5 hover:bg-gray-100 hover:border-gray-300 transition-colors'>
       <div className='mr-auto flex flex-row items-center gap-3'>
         <Image src={vehicle.path} alt='Vehicle Image' width={70} height={70} className='mr-10' />
         <div className='flex flex-col gap-1 justify-center truncate'>
-          <h1 className='text-2xl'>{ vehicle.brand } { vehicle?.type }</h1>
+          <div className='flex gap-3 items-center'>
+            <h1 className='text-2xl'>{ vehicle.brand } { vehicle?.type }</h1>
+            <p className='text-base px-3 py-1 bg-black text-white rounded-lg'>{ vehicle.category }</p>
+          </div>
           <p className='text-[#a0a0a0] text-base'>{ vehicle.plate }</p>
         </div>
         <div>
-          <p className='text-lg px-4 py-2 bg-black text-white rounded-lg'>{ vehicle.category }</p>
         </div>
       </div>
       <div className='flex flex-wrap gap-1'>
         <TooltipProvider>
           <Tooltip>
-            <TooltipTrigger className={buttonVariants({ variant: 'default' })} asChild>
-              <ModifyVehicleSheet vehicle={vehicle} />
-            </TooltipTrigger>
+            <ModifyVehicleSheet vehicle={vehicle} categories={categories} />
             <TooltipContent>Modify</TooltipContent>
           </Tooltip>
           <Tooltip>
