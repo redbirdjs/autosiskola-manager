022f3739f7ea36937eac932d37784d96442e7505
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-09 06:39:51 +0200

    feat: added categories to props, moved tooltip trigger to sheet

--- components/dashboard/vehicles/ModifyVehicleSheet.tsx
+++ components/dashboard/vehicles/ModifyVehicleSheet.tsx
@@ -1,20 +1,23 @@
 import { PenLine, PencilRuler, Car } from 'lucide-react'
 
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { TooltipTrigger } from '@/components/ui/tooltip'
 import { buttonVariants } from '@/components/ui/button'
 import ModifyVehicleForm from '@/components/dashboard/vehicles/ModifyVehicleForm'
-import { VehicleData } from '@/lib/definitions'
+import { VehicleData, CategoryName } from '@/lib/definitions'
 
-export default function ModifyVehicleSheet({ vehicle }: { vehicle: VehicleData }) {
+export default function ModifyVehicleSheet({ vehicle, categories }: { vehicle: VehicleData, categories: CategoryName[] }) {
   return (
     <Sheet>
-      <SheetTrigger className={buttonVariants({ variant: 'default' })}><PencilRuler className='h-5 w-5' /></SheetTrigger>
+      <TooltipTrigger asChild>
+        <SheetTrigger className={buttonVariants({ variant: 'default' })}><PencilRuler className='h-5 w-5' /></SheetTrigger>
+      </TooltipTrigger>
       <SheetContent>
         <SheetHeader className='mb-3'>
           <SheetTitle className='flex items-center gap-2'><PenLine className='h-5 w-5' /><Car className='h-5 w-5' />Modify Vehicle</SheetTitle>
           <hr />
         </SheetHeader>
-        <ModifyVehicleForm data={vehicle} />
+        <ModifyVehicleForm data={vehicle} categories={categories} />
       </SheetContent>
     </Sheet>
   );
