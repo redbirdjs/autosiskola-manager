f33e60fe98eacdcf30f803907fec90a7d2338f7d
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-10 17:39:14 +0200

    feat: created delete exam component

---
+++ components/dashboard/exams/DeleteExamButton.tsx
@@ -0,0 +1,11 @@
+import { Trash } from 'lucide-react'
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { deleteExam } from '@/utils/actions'
+
+export default function DeleteExam({ examId }: { examId: number }) {
+  return (
+    <DropdownMenuItem className='flex gap-2 text-red-600' onClick={() => deleteExam({ examId })}>
+      <Trash className='h-5 w-5' /> Delete Exam
+    </DropdownMenuItem>
+  );
+}
