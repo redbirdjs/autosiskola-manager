f9e08e8b74cdaf2d4427c5eb6b7966c7049a5f36
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-10 15:58:21 +0200

    chore: replaced dummy data with db

--- app/dashboard/payments/page.tsx
+++ app/dashboard/payments/page.tsx
@@ -1,24 +1,14 @@
-import { Payment, columns } from './columns'
+import { getPayments } from '@/utils/actions'
+
+import { columns } from './columns'
 import { DataTable } from '@/components/ui/data-table'
 
-export default function PaymentsPage() {
-  const data: Payment[] = [
-    {
-      id: 1,
-      name: 'Test Payment',
-      description: 'Online theory course',
-      student: 'Test Student|test_user',
-      issuer: 'Test Issuer',
-      amount: 10.11,
-      state: 0,
-      created: new Date(Date.now()-1200000),
-      due: new Date(Date.now()+99999999)
-    }
-  ];
+export default async function PaymentsPage() {
+  const payments = await getPayments();
 
   return (
     <div>
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={payments} />
     </div>
   );
 }
