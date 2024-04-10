fae73bda2e95ea9520680eb53cae40c2322984a6
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-10 18:05:02 +0200

    feat: moved set state button to their own components

---
+++ components/dashboard/exams/SetExamFailed.tsx
@@ -0,0 +1,17 @@
+import { CircleX } from 'lucide-react'
+import { setExamState } from '@/utils/actions'
+
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+
+export default function SetExamFailed({ examId, state }: { examId: number, state: number }) {
+  const setExamFailed = () => {
+    if (state != 0) return;
+    setExamState({ examId, state: 2 });
+  }
+
+  return (
+    <DropdownMenuItem className='flex gap-2' onClick={setExamFailed} disabled={state != 0}>
+      <CircleX className='h-5 w-5' /> Set Exam as Failed
+    </DropdownMenuItem>
+  );
+}
---
+++ components/dashboard/exams/SetExamPassed.tsx
@@ -0,0 +1,17 @@
+import { Check } from 'lucide-react'
+import { setExamState } from '@/utils/actions'
+
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+
+export default function SetExamPassed({ examId, state }: { examId: number, state: number }) {
+  const setExamPassed = () => {
+    if (state != 0) return;
+    setExamState({ examId, state: 1 });
+  }
+
+  return (
+    <DropdownMenuItem className='flex gap-2' onClick={setExamPassed} disabled={state != 0}>
+      <Check className='h-5 w-5' /> Set Exam as Passed
+    </DropdownMenuItem>
+  );
+}
