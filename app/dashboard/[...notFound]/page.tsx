6b57332059a504f0b06f695c525460d1210c099e
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-10 17:44:32 +0200

    chore: fixed text in not found page

--- app/dashboard/[...notFound]/page.tsx
+++ app/dashboard/[...notFound]/page.tsx
@@ -4,9 +4,9 @@ export default function NotFound() {
   return (
     <div className='w-10/12 flex items-center justify-center gap-4'>
       <Unplug className='w-20 h-20' /> 
-      <div className='flex flex-col items-center justify-center'>
+      <div className='flex flex-col gap-10 items-center justify-center'>
         <h1 className='flex flex-row gap-4 items-center text-6xl'>404</h1>
-        <p className='text-lg'>Ez az oldal nem található vagy még nem áll rendelkezésre!</p>
+        <p className='text-lg'>The page you are trying to reach is either not found or does not exist!</p>
       </div>
     </div>
   )
