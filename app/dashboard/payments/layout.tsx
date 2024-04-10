61ab1eef5d36522d1ca8e89fdacddb13862d3a94
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-10 09:34:34 +0200

    feat: added metadata to payments layout

--- app/dashboard/payments/layout.tsx
+++ app/dashboard/payments/layout.tsx
@@ -2,6 +2,10 @@ import { Metadata } from 'next'
 
 import DynamicBreadcrumb from '@/components/dashboard/DynamicBreadcrumb'
 
+export const metadata: Metadata = {
+  title: 'Payments | Dashboard'
+}
+
 export default function PaymentsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <main className='flex flex-col p-5 w-10/12'>
