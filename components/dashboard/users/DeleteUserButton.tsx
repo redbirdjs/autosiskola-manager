793a85354214c893a6ae8fc6df5e301263e54e99
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-12 12:07:05 +0200

    feat: created delete user button component

---
+++ components/dashboard/users/DeleteUserButton.tsx
@@ -0,0 +1,12 @@
+import { Trash2 as Trash } from 'lucide-react'
+
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { deleteUser } from '@/utils/actions'
+
+export default function DeleteUserButton({ username }: { username: string }) {
+  return (
+    <DropdownMenuItem className='flex gap-2 text-red-600' onClick={() => deleteUser({ username })}>
+      <Trash className='h-5 w-5' /> Delete User
+    </DropdownMenuItem>
+  );
+}
