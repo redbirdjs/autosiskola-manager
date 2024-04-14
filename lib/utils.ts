1b6e8ededab26ddebab9576dd77a8dfbb102078c
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-14 11:54:49 +0200

    feat: created get image provider function

--- lib/utils.ts
+++ lib/utils.ts
@@ -23,3 +23,13 @@ export function randomString(length: number, filter: RegExp = /[A-Za-z0-9]/g) {
 export function capitalizeLetter(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function getImageProvider() {
+  const externalProvider = process.env.IMAGE_PROVIDER || 'http://localhost:8080';
+  const developmentProvider = process.env.SITE_URL || 'http://localhost:3000';
+
+  if (process.env.NODE_ENV === 'development') {
+    return developmentProvider;
+  }
+  return externalProvider;
+}
