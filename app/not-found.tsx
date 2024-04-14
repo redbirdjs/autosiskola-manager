9b57a3eaa413517decce9e4cf249629cbb71b06a
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-14 10:59:46 +0200

    feat: added not found page for whole site

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+import { buttonVariants } from '@/components/ui/button'
+
+export default function NotFound() {
+  return (
+    <div className="w-screen h-screen flex flex-col gap-3 items-center justify-center">
+      <div className='flex'>
+        <h1 className='text-xl font-bold pr-3 mr-3 border-r border-r-[#eaeaea]'>404</h1>
+        <p>The page is not found!</p>
+      </div>
+      <Link href={'/'} className={buttonVariants({ variant: 'default' })}>Back to Main page</Link>
+    </div>
+  )
+}
