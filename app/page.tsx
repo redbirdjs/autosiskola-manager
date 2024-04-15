a52e2a07e95a38c78e2e8d9f4408d18ec922f854
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-15 16:31:48 +0200

    chore: main page redirects to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,4 @@
-import Link from 'next/link'
-import { buttonVariants } from '@/components/ui/button'
+import { redirect } from 'next/navigation'
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className='text-4xl mb-10'>Main page</h1>
-      <div className='flex gap-4'>
-        <Link href={'login'} className={buttonVariants({ variant: 'default' })}>Login Page</Link>
-        <Link href={'register'} className={buttonVariants({ variant: 'destructive' })}>Register Page</Link>
-        <Link href={'dashboard'} className={buttonVariants({ variant: 'outline' })}>To the Dashboard</Link>
-      </div>
-    </main>
-  );
+  redirect('/login');
 }
