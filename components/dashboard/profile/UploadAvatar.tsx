4a33b9ce99950c89eb8b43815ff46762d8161484
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-15 16:44:23 +0200

    feat: added image preview to upload

--- components/dashboard/profile/UploadAvatar.tsx
+++ components/dashboard/profile/UploadAvatar.tsx
@@ -1,19 +1,29 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import Image from 'next/image'
+import { useState, useEffect, useRef, ChangeEvent } from 'react'
 import { useFormState } from 'react-dom'
 import { uploadProfileAvatar } from '@/utils/actions'
 
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 import { Dialog, DialogTrigger, DialogHeader, DialogTitle, DialogContent, DialogClose, DialogFooter } from '@/components/ui/dialog'
 import { toast } from '@/components/ui/use-toast'
 
 export default function UploadAvatarForm({ userId }: { userId: number }) {
   const [dialogOpen, setDialogOpen] = useState(false);
+  const imageRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState<File>();
 
   const initialState = { message: { title: '' }, errors: {} };
   const [state, dispatch] = useFormState(uploadProfileAvatar, initialState);
+
+  const changeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setImage(e.target.files[0]);
+    }
+  }
 
   useEffect(() => {
     if (state.message.title.length > 0) {
@@ -39,7 +49,15 @@ export default function UploadAvatarForm({ userId }: { userId: number }) {
         <form action={dispatch}>
           <Input type='text' id='userId' name='userId' value={userId} className='hidden' readOnly />
           <label htmlFor='avatar'>Avatar</label>
-          <Input type='file' id='avatar' name='avatar' className='mt-1 mb-3' />
+          {
+            image && (
+              <Avatar className='mx-auto my-5 w-60 h-60'>
+                <AvatarImage src={URL.createObjectURL(image)} />
+                <AvatarFallback>Avatar Preview</AvatarFallback>
+              </Avatar>
+            )
+          }
+          <Input type='file' id='avatar' name='avatar' onChange={changeImage} className='mt-1 mb-3' />
           <DialogFooter>
             <DialogClose asChild>
               <Button type='submit'>Upload</Button>
