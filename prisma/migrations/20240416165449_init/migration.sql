7fef7f4fe5c4581a169cbe3be2d36f0dc66dabba
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-16 19:14:22 +0200

    build: added last migration for build stage

---
+++ prisma/migrations/20240416165449_init/migration.sql
@@ -0,0 +1,136 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "realName" VARCHAR(100) NOT NULL,
+    "passportNumber" VARCHAR(9) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "verifyToken" VARCHAR(64),
+    "avatarPath" VARCHAR(255) NOT NULL DEFAULT '/profiles/default.png',
+    "rankId" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ranks" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(9) NOT NULL,
+
+    CONSTRAINT "ranks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "calendar" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(50) NOT NULL,
+    "description" VARCHAR(100),
+    "date" TIMESTAMP NOT NULL,
+    "color" VARCHAR(7) NOT NULL DEFAULT '#ffffff',
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "calendar_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "courses" (
+    "id" SERIAL NOT NULL,
+    "theory" INTEGER NOT NULL DEFAULT 0,
+    "practise" INTEGER NOT NULL DEFAULT 0,
+    "finished" BOOLEAN NOT NULL DEFAULT false,
+    "studentId" INTEGER NOT NULL,
+    "teacherId" INTEGER NOT NULL,
+    "vehicleId" INTEGER,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "courses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" SERIAL NOT NULL,
+    "description" VARCHAR(100) NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "due" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "state" INTEGER NOT NULL DEFAULT 0,
+    "courseId" INTEGER NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "vehicles" (
+    "id" SERIAL NOT NULL,
+    "plate" VARCHAR(7) NOT NULL,
+    "color" TEXT DEFAULT 'white',
+    "driveType" TEXT NOT NULL DEFAULT 'manual',
+    "brand" VARCHAR(50) NOT NULL,
+    "type" VARCHAR(100) NOT NULL,
+    "imageUrl" VARCHAR(100) NOT NULL DEFAULT '/vehicles/fallback.png',
+    "categoryId" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "vehicles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "category" VARCHAR(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "exams" (
+    "id" SERIAL NOT NULL,
+    "date" TIMESTAMP NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "state" INTEGER NOT NULL DEFAULT 0,
+    "courseId" INTEGER NOT NULL,
+
+    CONSTRAINT "exams_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_passportNumber_key" ON "users"("passportNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_verifyToken_key" ON "users"("verifyToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "vehicles_plate_key" ON "vehicles"("plate");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_rankId_fkey" FOREIGN KEY ("rankId") REFERENCES "ranks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "calendar" ADD CONSTRAINT "calendar_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "courses" ADD CONSTRAINT "courses_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "courses" ADD CONSTRAINT "courses_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "courses" ADD CONSTRAINT "courses_vehicleId_fkey" FOREIGN KEY ("vehicleId") REFERENCES "vehicles"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "courses" ADD CONSTRAINT "courses_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "vehicles" ADD CONSTRAINT "vehicles_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "exams" ADD CONSTRAINT "exams_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
