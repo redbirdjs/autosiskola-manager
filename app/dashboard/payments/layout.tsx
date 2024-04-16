fd12c1dcd9eb46f0a454ed82f9ac694d215f2362
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-16 18:11:07 +0200

    chore: removed new button if rank is student

--- app/dashboard/payments/layout.tsx
+++ app/dashboard/payments/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import { getUserData } from '@/utils/user-actions'
 
 import DynamicBreadcrumb from '@/components/dashboard/DynamicBreadcrumb'
 import NewPaymentSheet from '@/components/dashboard/payments/NewPaymentSheet'
@@ -8,11 +9,13 @@ export const metadata: Metadata = {
 }
 
 export default async function PaymentsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const user = await getUserData();
+
   return (
     <main className='flex flex-col p-5 w-10/12'>
       <div className='flex flex-row justify-between mb-5'>
         <DynamicBreadcrumb />
-        <NewPaymentSheet />
+        { user?.rank.toLowerCase() != 'student' && <NewPaymentSheet /> }
       </div>
       { children }
     </main>
