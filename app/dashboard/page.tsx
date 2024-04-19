d3372562687b6d5a9bc42323fc2d457150b327ba
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-19 11:46:42 +0200

    feat: created main menu for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,54 @@
-import Link from 'next/link'
-
-import { buttonVariants } from '@/components/ui/button'
+import { redirect } from 'next/navigation'
 import { getUserData } from '@/utils/user-actions'
-import LogoutButton from '@/components/LogoutButton'
+
+import { Card, CardHeader, CardContent, CardTitle, CardDescription } from '@/components/ui/card'
+import { CalendarCard, ExamsCard, PaymentsCard, ProfileCard, StatisticsCard, TeacherExamsCard, VehiclesCard } from '@/components/dashboard/MainMenuCards'
 
 export default async function DashboardPage() {
   const user = await getUserData();
 
+  if (!user) return redirect('/');
+  const rank = user.rank.toLowerCase();
+
   return (
-    <main className='flex min-h-screen flex-col items-center p-24 w-10/12'>
-      <h1 className='text-4xl mb-10'>Dashboard Page</h1>
-      <p className='mb-3'>{ user?.username } && { user?.email }</p>
-      <Link href={'/'} className={buttonVariants({ variant: 'default' }) + ' mb-1'}>Back To Main Page</Link>
-      <LogoutButton />
+    <main className='min-h-screen flex flex-col gap-5 items-center p-5 w-10/12'>
+      <Card className='w-full'>
+        <CardHeader>
+          <CardTitle>Welcome to the Driving School Manager Dashboard!</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription className='text-base'>Here you can manage all your driving school related administrative tasks.</CardDescription>
+        </CardContent>
+      </Card>
+      <div className='w-full flex flex-row flex-wrap lg:flex-nowrap items-between gap-5'>
+        {
+          rank == 'student' && (
+            <>
+              <CalendarCard />
+              <ExamsCard />
+              <ProfileCard />
+            </>
+          )
+        }
+        {
+          rank == 'teacher' && (
+            <>
+              <TeacherExamsCard />
+              <PaymentsCard />
+              <ProfileCard />
+            </>
+          )
+        }
+        {
+          rank == 'admin' && (
+            <>
+              <VehiclesCard />
+              <StatisticsCard />
+              <ProfileCard />
+            </>
+          )
+        }
+      </div>
     </main>
   );
 }
