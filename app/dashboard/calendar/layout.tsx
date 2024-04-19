305756d896d2d693a322827b8babe83bdc733fe6
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-19 13:31:08 +0200

    feat: added new event sheet to calendar layout

--- app/dashboard/calendar/layout.tsx
+++ app/dashboard/calendar/layout.tsx
@@ -3,6 +3,8 @@ import { redirect } from 'next/navigation'
 import { getUserData } from '@/utils/user-actions'
 
 import DynamicBreadcrumb from '@/components/dashboard/DynamicBreadcrumb'
+import NewEventSheet from '@/components/dashboard/calendar/NewEventSheet'
+import { getStudentData } from '@/utils/course-actions'
 
 export const metadata: Metadata = {
   title: 'Calendar | Dashboard'
@@ -17,8 +19,9 @@ export default async function CalendarLayout({ children }: Readonly<{ children: 
 
   return (
     <main className='flex flex-col p-5 w-10/12'>
-      <div className='mb-5'>
+      <div className='flex flex-row justify-between mb-5'>
         <DynamicBreadcrumb />
+        { user?.rank.toLowerCase() == 'teacher' && <NewEventSheet /> }
       </div>
       { children }
     </main>
