a6e690faeb9f1b96b8488e7d2b7ba57d031cf6bb
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-19 10:54:29 +0200

    style: changed not found page style

--- app/dashboard/[...notFound]/page.tsx
+++ app/dashboard/[...notFound]/page.tsx
@@ -2,11 +2,10 @@ import { Unplug } from 'lucide-react'
 
 export default function NotFound() {
   return (
-    <div className='w-10/12 flex items-center justify-center gap-4'>
-      <Unplug className='w-20 h-20' /> 
-      <div className='flex flex-col gap-10 items-center justify-center'>
-        <h1 className='flex flex-row gap-4 items-center text-6xl'>404</h1>
-        <p className='text-lg'>The page you are trying to reach is either not found or does not exist!</p>
+    <div className='w-10/12 flex items-center justify-center gap-4 min-h-[600px]'>
+      <div className='flex'>
+        <h1 className='text-xl font-bold pr-3 mr-3 border-r border-r-[#eaeaea]'>404</h1>
+        <p>The dashboard page is not found!</p>
       </div>
     </div>
   )
