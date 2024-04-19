5ed4097f9cee4fcf5a8aceef6e9c25d388186162
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-19 08:54:47 +0200

    feat: added change forgotten password schema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -5,7 +5,7 @@ export const RegisterSchema = z.object({
   realname: z.string().min(1, 'This field is required!'),
   email: z.string().min(1, 'This field is required!').email({ message: 'Invalid email format!' }),
   passport: z.string().min(1, 'This field is required!').regex(/^[A-Z0-9]{8,9}$/, 'Invalid passport number!'),
-  pass1: z.string().min(1, 'This field is required!').min(8, 'The password must be at least 8 characters long.'),
+  pass1: z.string().min(1, 'This field is required!').min(8, 'The password must be at least 8 characters long!'),
   pass2: z.string().min(1, 'This field is required!'),
 });
 
@@ -16,6 +16,12 @@ export const LoginSchema = z.object({
 
 export const PasswordReminderSchema = z.object({
   email: z.string().min(1, 'This field is required!').email({ message: 'Invalid email format!' }),
+});
+
+export const ChangeForgottenPasswordSchema = z.object({
+  passToken: z.string().min(1, 'The token is required for changing the password!'),
+  pass1: z.string().min(1, 'This field is required!').min(8, 'The password must be at least 8 characters long!'),
+  pass2: z.string().min(1, 'This field is required!')
 });
 
 export const UserSchema = z.object({
