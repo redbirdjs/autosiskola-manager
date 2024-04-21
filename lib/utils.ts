e263b44bd701c29c014c7c49dcd5c4a390e26bec
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-21 12:58:55 +0200

    chore: added param for capilatizing all starting letters

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,13 +16,22 @@ export function randomString(length: number, exclude: RegExp = /[^A-Za-z0-9]/g) 
   let chars = 'aA0bB1cC2dD3eE4fF5gG6hH7iI8jJ9kKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ'.replaceAll(exclude, '');
 
   for (let i = 0; i < length; i++) {
-      str += chars.charAt(Math.floor(Math.random() * chars.length));
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
   }
 
   return str;
 }
 
-export function capitalizeLetter(str: string) {
+/**
+ * Egy funkció ami a beadott string első karakterét / karaktereit nagybetűssé alakítja.
+ * @param str String aminek az első karakterét nagybetűssé szeretnénk alakítani.
+ * @param capAll Az összes kezdő karakter nagybetűs legyen-e.
+ * @returns Nagybetűvel kezdődő string.
+ */
+export function capitalizeLetter(str: string, capAll: boolean = false) {
+  if (capAll) {
+    return str = str.split(' ').map(s => s[0].toUpperCase() + s.slice(1)).join(' ');
+  }
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
