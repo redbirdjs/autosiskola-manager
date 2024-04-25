3b5a6a95df4dff1419ee7b959569d02068e5937c
Mátrai Gergő <35497481+redbirdjs@users.noreply.github.com>
2024-04-25 17:50:04 +0200

    feat: added course action components

---
+++ components/dashboard/courses/CourseActions.tsx
@@ -0,0 +1,47 @@
+import { BookCheck, FilePenLine, Trash2 as Trash } from 'lucide-react'
+import { setCourseToFinished, deleteCourse } from '@/utils/course-actions'
+
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { toast } from '@/components/ui/use-toast'
+
+async function setCourseToFin(id: number) {
+  const result = await setCourseToFinished(id);
+  toast({
+    title: result.message.title,
+    description: result.message.description,
+    duration: 2000
+  });
+}
+
+async function delCourse(id: number) {
+  const result = await deleteCourse(id);
+  toast({
+    title: result.message.title,
+    description: result.message.description,
+    duration: 2000
+  });
+}
+
+export function SetCourseFinished({ state, courseId }: { state: boolean, courseId: number }) {
+  return (
+    <DropdownMenuItem className='flex gap-2' disabled={state} onClick={() => setCourseToFin(courseId)}>
+      <BookCheck className='h-5 w-5' /> Set Course as Finished
+    </DropdownMenuItem>
+  );
+}
+
+export function ModifyCourseData({ courseId }: { courseId: number }) {
+  return (
+    <DropdownMenuItem className='flex gap-2'>
+      <FilePenLine className='h-5 w-5' /> Modify Course
+    </DropdownMenuItem>
+  );
+}
+
+export function DeleteCourse({ courseId }: { courseId: number }) {
+  return (
+    <DropdownMenuItem className='flex gap-2 text-red-600' onClick={() => delCourse(courseId)}>
+      <Trash className='h-5 w-5' /> Delete Course
+    </DropdownMenuItem>
+  );
+}
